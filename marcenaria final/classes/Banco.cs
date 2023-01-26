69fb80b4122f8c597feac4d351b98b34080d3f96
Felipe Stempkowski <97076665+FelipeStemp@users.noreply.github.com>
2023-01-26 02:05:23 -0300

    Add files via upload

---
+++ marcenaria final/classes/Banco.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.SqlClient;
+
+
+namespace marcenaria_final.classes
+{
+    public class Banco
+    {
+        public SqlConnection con = new SqlConnection("CONEXAO SQL");
+    }
+}
